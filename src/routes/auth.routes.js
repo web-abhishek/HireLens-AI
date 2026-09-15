2e3c3a086e2556ae05d66203e2cfcc2c1676a457
web-abhishek <web.abhiacharya@gmail.com>
2026-09-15 14:58:55 +0530

    Add user registration functionality with JWT authentication

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRouter = express.Router();
 const authcontroller = require('../controllers/auth.controller');
 
+// For register a new user
 authRouter.post('/register', authcontroller.registerUserController);
 
 
