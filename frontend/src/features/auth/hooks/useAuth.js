f37bc86736213abe71f967da372edc2fe392620a
web-abhishek <web.abhiacharya@gmail.com>
2026-09-24 23:08:49 +0530

    feat: implement authentication functions in useAuth hook for login, registration, logout, and user retrieval

--- frontend/src/features/auth/hooks/useAuth.js
+++ frontend/src/features/auth/hooks/useAuth.js
@@ -1,7 +1,33 @@
 import { useContext } from "react"
 import { AuthContext } from "../context/auth.context"
-
+import { login, register, logout, getMe } from "../services/auth.api";
 export const useAuth = () => {
     const context = useContext(AuthContext);
     const { user, setUser, loading, setLoading } = context;
+
+    const handleLogin = async ({ email, password }) => {
+        setLoading(true);
+        const data = await login({ email, password })
+        setUser(data.user)
+        setLoading(false)
+    }
+    const handleRegister = async ({ username, email, password }) => {
+        setLoading(true);
+        const data = await register({ username, email, password })
+        setUser(data.user)
+        setLoading(false)
+    }
+    const handleLogout = async () => {
+        setLoading(true);
+        const data = await logout()
+        setUser(data.user)
+        setLoading(null)
+    }
+    const handleGetme = async ({ email, password }) => {
+        setLoading(true);
+        const data = await getme({ email, password })
+        setUser(data.user)
+        setLoading(false)
+    }
+
 }
